2e8b816db98c280a4708f4fff29e2cbf146aa14f
LIU-Xiyuan <xliuba@connect.ust.hk>
2016-09-29 17:36:18 +0800

    modify function fRigid.m

--- 2D image transforms/fRigid.m
+++ 2D image transforms/fRigid.m
@@ -4,9 +4,9 @@ function y = fRigid(x,pIn)
 theta = x(1);
 tx = x(2);
 ty = x(3);
-H = [ cos(theta) -sin(theta) tx;
- sin(theta) cos(theta) ty;
- 0 0 1];
+H = [cos(theta) -sin(theta) tx;
+     sin(theta) cos(theta)  ty;
+     0          0           1];
 pOut = H*pIn;
 pOut = pOut(1:2, :); % 1st two rows
 y = reshape(pOut,[],1);
